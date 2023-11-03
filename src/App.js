7de434ad0eba77ae325ef2c546f09cebae6252b6
Sniknam <sianiknam@gmail.com>
2023-11-02 18:13:50 -0700

    start to create navigation bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Outlet } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>im the navigation bar</h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+};
+const Shop = () => {
+  return <h1>im in shop page</h1>;
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Navigation />}>
+        <Route path="home" element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        {/* /home/shop */}
+      </Route>
     </Routes>
   );
 };
