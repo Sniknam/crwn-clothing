a94c7312d0e97492b62875e34fff69d27e08ea25
Sniknam <sianiknam@gmail.com>
2023-11-04 11:49:37 -0700

    create navigtion componenet and add the logo and shop to it as link.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import { Routes, Route, Outlet } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
 
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>im the navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from "./routes/navigation/navigation.component";
+
 const Shop = () => {
   return <h1>im in shop page</h1>;
 };
+
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
-        <Route path="home" element={<Home />} />
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
         {/* /home/shop */}
       </Route>
