aaa4e507fced4618092af6f0706b641d5e1171ac
Sniknam <sianiknam@gmail.com>
2023-11-08 17:32:45 -0800

    update the signin copmonent with google sign in future

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,7 +1,17 @@
+
+import {signInWithGooglePopup  } from '../utils/firebase/firebase.utils'
 const SignIn= ()=>{
+    const logGoogleUser = async () =>{
+        const responce= await signInWithGooglePopup ()
+
+        console.log(responce)
+    }
     return (
         <div>
             <h1>sign in page</h1>
+            <button onClick={logGoogleUser}>
+                sign in with google
+            </button>
         </div>
     )
 }
