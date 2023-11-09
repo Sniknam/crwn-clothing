bdb07e05528e625d058d67b312838458e6b04635
Sniknam <sianiknam@gmail.com>
2023-11-08 16:56:28 -0800

    set up the sign in auth with googgle

---
+++ src/routes/utils/firebase/firebase.utils.js
@@ -0,0 +1,29 @@
+import { initializeApp } from "firebase/app";
+
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyArnIqAh_I1fCzXzZiV6YMwTXCA1l34YHY",
+  authDomain: "crwn-clothing-fdd03.firebaseapp.com",
+  projectId: "crwn-clothing-fdd03",
+  storageBucket: "crwn-clothing-fdd03.appspot.com",
+  messagingSenderId: "140404776627",
+  appId: "1:140404776627:web:a61c315cf02df3b9b79ac6",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
