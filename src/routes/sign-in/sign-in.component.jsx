a6371a22d6681180bc06695f8ddfd2ed82b0f5ab
Sniknam <sianiknam@gmail.com>
2023-11-27 18:03:42 -0800

    making onother btn to sign in with google redirectly.

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,14 @@
 
-import { useReducer } from 'react'
-import {signInWithGooglePopup,creatuserDocumentFromAuth } from '../utils/firebase/firebase.utils'
+
+import {signInWithGooglePopup,creatuserDocumentFromAuth,signInWithGoogleRedirect } from '../utils/firebase/firebase.utils'
 const SignIn= ()=>{
     const logGoogleUser = async () =>{
         const {user}= await signInWithGooglePopup ()
+        const userDocRef= await creatuserDocumentFromAuth(user)
+       
+    }
+    const logGoogleUserRedirct = async () =>{
+        const {user}= await signInWithGoogleRedirect ()
         const userDocRef= await creatuserDocumentFromAuth(user)
        
     }
@@ -12,6 +17,9 @@ const SignIn= ()=>{
             <h1>sign in page</h1>
             <button onClick={logGoogleUser}>
                 sign in with google
+            </button>
+            <button onClick={logGoogleUserRedirct}>
+                sign in with google Redirect
             </button>
         </div>
     )
